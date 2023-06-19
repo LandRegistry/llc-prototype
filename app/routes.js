a96995649f972fddf768776a3d0244d90a936220
Martin Coulthurst <mc@lightness.co.uk>
2023-06-19 13:06:56 +0100

    Add old prototype

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,28 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+// Clear session data without confirmation
+router.get('/prototype-admin/clear-data', function (req, res) {
+  req.session.destroy()
+  res.redirect('/')
+})
+
+// Grab the feature urls for the aside heroku links
+router.use('/', (req, res, next) => {
+  res.locals.feature = req.url
+  if (req.get('host').includes('localhost')) {
+    res.locals.host = true
+  }
+  next()
+})
+
+// Import routes from feature prototypes
+router.use(/\/(.)*\/(.)*\/v([0-9]+)/, (req, res, next) => {
+  return require(`./views/${req.originalUrl.split('/')[1]}/${req.originalUrl.split('/')[2]}/v${req.params[2]}/_routes`)(req, res, next);
+})
+
+// Redirect the org switcher so  it works at different levels
+router.get('*/org-switch', function (req, res) {
+  res.redirect(`/${req.originalUrl.split('/')[1]}/${req.originalUrl.split('/')[2]}/${req.originalUrl.split('/')[3]}/local-authorities`)
+})
