9d94e9a011fb7a460bb453a8dcc0f28344358342
Martin Coulthurst <mc@lightness.co.uk>
2023-12-01 15:10:49 +0000

    Revise contact form content

--- app/assets/ol/js/map_styles.js
+++ app/assets/ol/js/map_styles.js
@@ -131,10 +131,10 @@ draw_layer_styles = {
     // HOVER
     5:  new ol.style.Style({
             fill: new ol.style.Fill({
-              color: 'rgba(255,255,255,0.1)'
+              color: 'rgba(76,44,146,0.3)'//'rgba(255,255,255,0.1)'
             }),
             stroke: new ol.style.Stroke({
-              color: '#4c2c92',//'#0b0c0c',
+              color: 'rgba(76,44,146,1)',//'#0b0c0c',
               width: 2
             }),
             radius: 5
