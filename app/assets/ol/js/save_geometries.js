21d05a2e11dd276d899913050c9373228d267cde
Martin Coulthurst <mc@lightness.co.uk>
2024-09-02 15:42:16 +0100

    Clear map when it first loads, add find button function to move map

--- app/assets/ol/js/save_geometries.js
+++ app/assets/ol/js/save_geometries.js
@@ -52,3 +52,47 @@ function load_previous_data(information) {
         }
     }
 }
+function load_fake_data(information) {
+    if(information) {
+        try {
+            var options = {
+                dataProjection: 'EPSG:27700',
+                featureProjection: 'EPSG:27700'
+            }
+
+            var features = new ol.format.GeoJSON().readFeatures(information, options);
+
+            MAP_CONFIG.draw_source.addFeatures(features);
+            // default style without hatching
+            //MAP_CONFIG.draw_layer.setStyle(draw_layer_styles.style[draw_layer_styles.DRAW])
+
+            if (features.length == 1 && features[0].getGeometry().getType() == "Point"){
+                map.getView().animate({
+                    center: features[0].getGeometry().getCoordinates(),
+                    zoom: 100,
+                    duration: 1
+                });
+            } else {
+                var extent = features[0].getGeometry().getExtent().slice(0);
+
+                features.forEach(function (feature) {
+                    ol.extent.extend(extent, feature.getGeometry().getExtent());
+                });
+
+                map.getView().fit(extent);
+
+                // get the map zoom with the new extents
+                const view = map.getView();
+                const zoom = view.getZoom();
+                // then zoom out slightly to show the context / surrounding area
+                let coords =[532050, 181750];
+                 view.animate({
+                  center: coords,
+                  zoom: (zoom + 1.8),
+                  duration: 0
+                }); 
+            }
+        } catch (e) {
+        }
+    }
+}
