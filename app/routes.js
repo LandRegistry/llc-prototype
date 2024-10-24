dd3dbe13da85274243ecb5d474f37b49318577b3
Martin Coulthurst <mc@lightness.co.uk>
2024-10-24 15:15:54 +0100

    Include route for view charge to make page dynamic

--- app/routes.js
+++ app/routes.js
@@ -26,11 +26,13 @@ router.use('/', (req, res, next) => {
 })
 
 router.get('/search/sprint-77/find-a-charge/view-charge', function(req, res) {
-  // Attempt to render a page in the current folder
-  // console.log("------- routes.js ------ ");
-  // console.log(req.body); 
-
   res.render('/search/sprint-77/find-a-charge/view-charge', {
+    index: res.locals.data.index 
+  });
+});
+
+router.get('/search/sprint-77/find-a-charge/view-charge-v2', function(req, res) {
+  res.render('/search/sprint-77/find-a-charge/view-charge-v2', {
     index: res.locals.data.index 
   });
 });
